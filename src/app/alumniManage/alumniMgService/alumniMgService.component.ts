5ed64f8bd8ec59c65f2eb968bb77bd26796967c7
Hubmeat_tl <satantanqiuqiu@163.com>
2018-03-24 16:52:59 +0800

    modify waitPending Com

--- src/app/alumniManage/alumniMgService/alumniMgService.component.ts
+++ src/app/alumniManage/alumniMgService/alumniMgService.component.ts
@@ -16,23 +16,42 @@ export class AMService {
         
     }
 
+    /**
+     * 校友管理模块
+     */
+
+
+    // 校友管理待审核subject
     public WaitPendingSubject = new Subject<any>();
 
-    public getPendingData():void {
+    public getPendingData(
+        list_type, 
+        majorSelected,
+        educationSelected,
+        collegeSelected,
+        registerBeginTime,
+        registeEndrTime,
+        joinBeginTime,
+        joinEndrTime,
+        userName,
+        IDcard,
+        phone,
+        currentPage
+    ):void {
         var formData = {
-            "list_type": 1,
-            "education": "专科",
-            "page": 1,
-            "profession_id": 1705616188964869,
-            "academy_id": 1705577148383233,
-            "registe_start": 1521724069159,
-            "registe_end": 1521724069159,
-            "academic_start": 1521705972777,
-            "academic_end": 1521705972777,
-            "user_name": "小明",
-            "id_number": "140332122333233321",
-            "contact_phone": "18333608366",
-            "examine_state": 1
+            "list_type": list_type,
+            "education": educationSelected,
+            "page": currentPage,
+            "profession_id": majorSelected,
+            "academy_id": collegeSelected,
+            "registe_start": registerBeginTime === null?'':moment(registerBeginTime).valueOf(),
+            "registe_end": registeEndrTime === null?'':moment(registeEndrTime).valueOf(),
+            "academic_start": joinBeginTime === null?'':moment(joinBeginTime).valueOf(),
+            "academic_end": joinEndrTime === null?'':moment(joinEndrTime).valueOf(),
+            "user_name": userName,
+            "id_number": IDcard,
+            "contact_phone": phone,
+            "examine_state": ''
         };
 
         this.$http
@@ -43,6 +62,41 @@ export class AMService {
                 },
                 err => {
                     this.WaitPendingSubject.next(err)
+                }
+            )
+    }
+
+    // 校友管理待审核，下拉框subject
+    public waitPendingOfSelectSubject = new Subject<any>();
+
+    public getCollegeSelectData():void {        
+        // 获取 学院 下拉框相关数据
+        this.$http
+            .post(this.$HOST.host + '/util/academy/dropbox', {})
+            .subscribe(
+                res => {
+                    this.waitPendingOfSelectSubject.next({college: res})
+                },
+                err => {
+                    this.waitPendingOfSelectSubject.next({college: err})
+                }
+            )
+    }
+
+    public getMajorSelectData(value):void {
+        // 获取 专业 下拉框相关数据
+        var formData = {
+            academy_id: value
+        }
+
+        this.$http
+            .post(this.$HOST.host + '/util/major/dropbox', formData)
+            .subscribe(
+                res => {
+                    this.waitPendingOfSelectSubject.next({major: res})
+                },
+                err => {
+                    this.waitPendingOfSelectSubject.next({major: err})
                 }
             )
     }
