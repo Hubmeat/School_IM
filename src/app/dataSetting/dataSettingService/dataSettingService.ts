729dad89a238d15222ae609479918c48476c63ac
Guoyuan <243341386@qq.com>
2018-03-24 18:02:45 +0800

    add 资料设置

--- src/app/dataSetting/dataSettingService/dataSettingService.ts
+++ src/app/dataSetting/dataSettingService/dataSettingService.ts
@@ -3,9 +3,55 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/retry';
+import {ApiModule} from '../../api/api';
 
 @Injectable()
 
 export class DataSettingService {
+    constructor(
+      private $HOST: ApiModule,
+      private $http: HttpClient
+    ) {}
 
+  /**
+   *学院信息
+   */
+  public DataSettingDepartmentsSubject = new Subject<any>();
+    // 搜索/查询list
+    public getDepartmentsList(page, searchParam) {
+      const formData = {
+        page: 1,
+        searchParam: '电'
+      }
+      this.$http
+        .post(this.$HOST.host + '/a/academy/list', formData)
+        .subscribe(res => {
+          this.DataSettingDepartmentsSubject.next({'dataList': res})
+        })
+    }
+
+    // 添加学院信息
+    public addDepartments(uid, academy_name) {
+      const formData = {
+        uid: 1646334584094728,
+        academy_name: academy_name
+      }
+      this.$http
+        .post(this.$HOST.host + '/a/academy/new', formData)
+        .subscribe(res => {
+          this.DataSettingDepartmentsSubject.next({addmsg: res})
+        })
+    }
+
+    // 删除学院信息
+    public deleteDepartments(id) {
+      const formData = {
+        id: 123131
+      };
+      this.$http
+        .post(this.$HOST.host + '/a/academy/remove', formData)
+        .subscribe(res => {
+          this.DataSettingDepartmentsSubject.next({delmsg: res})
+        })
+    }
 }
--- src/app/dataSetting/departmentsSetup/departmentsSetup.component.ts
+++ src/app/dataSetting/departmentsSetup/departmentsSetup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataSettingService} from '../dataSettingService/dataSettingService';
+import {NzMessageService} from 'ng-zorro-antd';
 
 @Component({
   selector: 'departmentsSetup-component',
@@ -10,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 
 export class DepartmentsSetupComponent implements OnInit {
   _dataSet = [];
-  schoolName = ''
-  isVisible = false
-  isVisible1= false
+  schoolName = '';
+  isVisible = false;
+  isVisible1= false;
+  page: 1;
+  searchParam: '';
+
+  academy_name: string = ''; // 学院名
+  msg: string = '';
+  constructor(
+    private service: DataSettingService,
+    private _message: NzMessageService,
+  ) {}
   ngOnInit() {
     for (let i = 0; i < 46; i++) {
       this._dataSet.push({
@@ -22,7 +33,58 @@ export class DepartmentsSetupComponent implements OnInit {
         address: `London, Park Lane no. ${i}`,
       });
     }
+
+    // this.search()
   }
+
+  search() {
+    this.service.getDepartmentsList(
+      this.page,
+      this.searchParam
+    );
+    this.service.DataSettingDepartmentsSubject.subscribe(res => {
+      console.log(res);
+    })
+  }
+
+  // 添加学院信息
+  addDepartments() {
+    if (this.academy_name  === '') {
+      this._message.warning(`学院名称为空!`);
+      return;
+    }
+    const uid = window.localStorage.getItem('id');
+    this.service.addDepartments(
+      uid,
+      this.academy_name
+    );
+    this.service.DataSettingDepartmentsSubject.subscribe(res => {
+      console.log(res);
+      if (res.addmsg.error_code !== 0) {
+        this.msg = res.addmsg.error_msg;
+        this._message.success(this.msg || '添加失败');
+      }else {
+        this._message.success(`添加成功!`);
+      }
+    })
+  }
+
+  // 删除学院信息
+  deleteDepartments(id) {
+    this.service.deleteDepartments(
+      id
+    );
+    this.service.DataSettingDepartmentsSubject.next(res => {
+      console.log(res);
+      if (res.delmsg.error_code !== 0) {
+        this._message.warning('删除失败');
+      } else {
+        this._message.success('删除成功');
+      }
+    })
+  }
+
+
   showModal = () => {
     this.isVisible = true;
   }
