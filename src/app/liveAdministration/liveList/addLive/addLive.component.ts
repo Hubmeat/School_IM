bfa5252291f585bcc06f2f3f15a026c7cb2b9260
Guoyuan <243341386@qq.com>
2018-04-01 21:46:34 +0800

    add 添加 - 直播模块.暂缺编辑

--- src/app/liveAdministration/liveList/addLive/addLive.component.ts
+++ src/app/liveAdministration/liveList/addLive/addLive.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NzMessageService} from 'ng-zorro-antd';
+import {LiveAdministrationService} from '../../liveAdministrationService/liveAdministration.service';
+import {Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription'
 
 
 @Component({
@@ -11,42 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AddLiveComponent implements OnInit  {
-  _dataSet = [];
-  isVisible = false
-  isVisible1= false
-  newUserName
-  avatarUrl
-  radioValue
-  beforeUpload
+  live_title: string = '';
+  live_person: string = '';
+  video_url: string = '';
+  avatarUrl: string = '';
+  video_type: string = '1';
+  live_state: string = '1';
+  live_pic;
+  live_person_id;
+  live_person_gender;
+  addSubscription: Subscription // 列表
+
+  constructor(
+    private service: LiveAdministrationService,
+    private _message: NzMessageService,
+    private router: Router
+  ) {}
   ngOnInit() {
-    for (let i = 0; i < 46; i++) {
-      this._dataSet.push({
-        key    : i,
-        name   : `Edward King ${i}`,
-        age    : 32,
-        address: `London, Park Lane no. ${i}`,
-      });
-    }
-  }
-  showModal = () => {
-    this.isVisible = true;
-  }
-  showModal1 = () => {
-    this.isVisible1 = true;
+
   }
 
-  handleOk = (e) => {
-    console.log('点击了确定');
-    this.isVisible = false;
-    this.isVisible1 = false;
+  goback() {
+    this.router.navigate(['/index/liveList']);
   }
+  submitLive() {
+    const uid = window.localStorage.getItem('uid');
 
-  handleCancel = (e) => {
-    console.log(e);
-    this.isVisible = false;
-    this.isVisible1 = false;
-  }
-  handleChange = (e) => {
-
+    this.service.addLiveList(
+      uid,
+      this.live_title,
+      this.live_pic,
+      this.live_person_id,
+      this.live_person,
+      this.live_person_gender,
+      this.live_state,
+      this.video_url,
+      this.video_type
+    )
+    this.addSubscription = this.service.LiveListSubject.subscribe(res => {
+      console.log(res);
+      if (res.addmsg === undefined) {
+        return;
+      }
+      if (res.addmsg.error_code === 0) {
+        this._message.success('添加成功');
+        this.router.navigate(['/index/liveList']);
+        this.addSubscription.unsubscribe();
+      }
+    })
   }
 }
--- src/app/liveAdministration/liveList/liveList.component.ts
+++ src/app/liveAdministration/liveList/liveList.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LiveAdministrationService} from '../liveAdministrationService/liveAdministration.service';
 import {NzMessageService} from 'ng-zorro-antd';
+import {Subscription} from 'rxjs/Subscription'
 
 
 
@@ -14,64 +15,128 @@ import {NzMessageService} from 'ng-zorro-antd';
 
 export class LiveListComponent implements OnInit  {
   _dataSet = [];
-  isVisible = false
-  isVisible1= false
-  newUserName
-  selectedOption
-  options
+  isDetailsModal = false; // 详情弹窗
+  detailData: any = {
+    'id': 0,
+    'uid': 0,
+    'live_title': '',
+    'live_pic': '',
+    'live_time': 0,
+    'live_person_id': 0,
+    'live_person': 0,
+    'live_person_gender': '',
+    'publish_person': '',
+    'foreshow_intro': '',
+    'video_url': '',
+    'video_type': 0,
+    'live_state': 0,
+    'live_type': 0
+  }; // 详情数据
+  options = [
+    {
+      label: '直播',
+      value: '1'
+    },
+    {
+      label: '回放',
+      value: '2'
+    }
+  ]; // 视频状态下拉列表
 
-  searchParam: string;
-  page: 1;
-  live_title: string; // 直播标题
+  searchParam: string = '';
+  page: number = 1;
+  live_title: string = ''; // 直播标题
   video_type: string; // 视频类型：1 代表直播 2 代表回放
+
+  listSubscription: Subscription // 列表
+  delSubscription: Subscription; // 删除
+  togSubscription: Subscription; // 切换状态
+
 
   constructor(
     private service: LiveAdministrationService,
-    private _message: NzMessageService
-
+    private _message: NzMessageService,
   ) {}
 
   ngOnInit() {
-    for (let i = 0; i < 46; i++) {
-      this._dataSet.push({
-        key    : i,
-        name   : `Edward King ${i}`,
-        age    : 32,
-        address: `London, Park Lane no. ${i}`,
-      });
-    }
     this.loadData();
   }
 
   loadData() {
+    console.log(this.video_type)
     this.service.geiLiveListDataList(
       this.searchParam,
       this.live_title,
       this.page,
       this.video_type
     )
-    this.service.LiveListSubject.subscribe(res => {
+    this.listSubscription = this.service.LiveListSubject.subscribe(res => {
       console.log(res);
-      this._dataSet = res.dataList.result;
+      if (res.dataList === undefined) {
+        return;
+      }
+      if (res.dataList.error_code === 0) {
+        this._dataSet = res.dataList.result;
+        this.listSubscription.unsubscribe();
+      }
+    })
+  }
+  // 详情
+  showDetailsModal = (id, index) => {
+    this.isDetailsModal = true;
+    this.service.geiLivePreDetail(id);
+    this.service.LiveServiceSubject.subscribe(res => {
+      console.log(index);
+      if (res.detail === undefined) {
+        return;
+      }
+      if (res.detail.error_code === 0) {
+        this.detailData = res.detail.result[index];
+      }
     })
   }
 
-  showModal = () => {
-    this.isVisible = true;
+  // 删除
+  deleteLive(id) {
+    this.service.deleteLivePre(id);
+    this.delSubscription = this.service.LiveServiceSubject.subscribe(res => {
+      console.log(res);
+      if (res.delmsg === undefined) {
+        return
+      }
+      if (res.delmsg.error_code === 0) {
+        this.loadData();
+        this._message.success('删除成功');
+        this.delSubscription.unsubscribe();
+      }
+    })
   }
-  showModal1 = () => {
-    this.isVisible1 = true;
+
+  // 切换状态
+  toggleState(live_state, id) {
+    const state = live_state === 1 ? 2 : 1;
+    this.service.LivePreState(state, id);
+    this.togSubscription = this.service.LiveServiceSubject.subscribe(res => {
+      console.log(res);
+      if (res.statemsg === undefined) {
+        return;
+      }
+      if (res.statemsg.error_code === 0) {
+        this._message.success('切换成功');
+        this.togSubscription.unsubscribe();
+        this.loadData();
+      }
+    })
   }
 
   handleOk = (e) => {
-    console.log('点击了确定');
-    this.isVisible = false;
-    this.isVisible1 = false;
+    this.isDetailsModal = false;
   }
-
+  deleteLivecancel() {
+    this.isDetailsModal = false;
+  }
   handleCancel = (e) => {
     console.log(e);
-    this.isVisible = false;
-    this.isVisible1 = false;
+    this.isDetailsModal = false;
   }
 }
